30f14fcbbc7bcc6a527df27b87936018e9f90a6c
Prince Pius <princenjr111@gmail.com>
2025-01-24 14:03:12 +0530

    frontend

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { serverUrl } from '../api';
 
 const Menu = () => {
   const [menus, setMenus] = useState([]);
